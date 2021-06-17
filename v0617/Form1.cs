5f212677862753cdd39d99f8e9be251321176641
ozeki <hhdfgg@users.noreply.github.com>
2021-06-17 09:55:58 +0900

    Initial commit

---
+++ v0617/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace v0617
+{
+    public partial class Form1 : Form
+    {
+
+        int vx = -10, vy = -10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(vx < 0){
+                vx = 10;
+            }
+            if(vy < 0){
+                vy = 10;
+            }
+            if (vx > ClientSize.Width)
+            {
+                vx = -10;
+            }
+            if (vy > ClientSize.Height)
+            {
+                vy = -10;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+        }
+    }
+}
